e32f4d105f36c34de5f60ebedd502bf28c0d2ef0
pankaj-pant <pankajpant89@gmail.com>
2020-08-10 20:46:44 +0300

    ex 9.3

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,18 +1,29 @@
 const calculateBmi = (height: number, weight: number) => {
-    const result = ((weight / (height * height)) * 10000)
-    let bmi;
-    switch (true) {
-        case (result < 25):
-            bmi = "Normal (healthy weight)";
-            break;
-        case (result >= 30):
-            bmi = "obese";
-            break;
-        default:
-            bmi = "overweight";
-            break;
+    if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+        const result = ((weight / (height * height)) * 10000)
+        let bmi;
+        switch (true) {
+            case (result < 25):
+                bmi = "Normal (healthy weight)";
+                break;
+            case (result >= 30):
+                bmi = "obese";
+                break;
+            default:
+                bmi = "overweight";
+                break;
+        }
+        return bmi;
+    } else {
+        throw new Error('Provided values were not numbers!');
     }
-    console.log(bmi)
 }
 
-calculateBmi(180, 94)
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+
+try {
+    console.log(calculateBmi(height, weight))
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+}
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,41 +8,51 @@ interface ResultObject {
     average: number,
 }
 
+const calculateExercises = (target: number, hours: number[]): ResultObject => {
 
-const calculateExercises = (hours: number[], target: number): ResultObject => {
-
-    const periodLength = hours.length;
-    const trainingDays = hours.filter(a => a > 0);
-    const sum = hours.reduce((a, b) => a + b, 0)
-    const average = (sum / periodLength)
-    const success = average >= target ? true : false;
-    let rating;
-    let ratingDescription;
-    const calcTraining = () => {
-        if (average >= target){
-            rating = 3;
-            ratingDescription = "Well done!"
-        } else if (target - average < 0.3){
-            rating = 2;
-            ratingDescription = "not too bad but could be better"
-        } else {
-            rating = 1;
-            ratingDescription = "You need to put more effort in!"
+    if (hours.every(h => !isNaN(Number(h))) && !isNaN(Number(target))) {
+        const periodLength = hours.length;
+        const trainingDays = hours.filter(a => a > 0);
+        const sum = hours.reduce((a, b) => a + b, 0)
+        const average = (sum / periodLength)
+        const success = average >= target ? true : false;
+        let rating;
+        let ratingDescription;
+        const calcTraining = () => {
+            if (average >= target){
+                rating = 3;
+                ratingDescription = "Well done!"
+            } else if (target - average < 0.3){
+                rating = 2;
+                ratingDescription = "not too bad but could be better"
+            } else {
+                rating = 1;
+                ratingDescription = "You need to put more effort in!"
+            }
         }
+        calcTraining();
+    
+        return {
+            periodLength: periodLength,
+            trainingDays: trainingDays.length,
+            success: success,
+            rating: rating,
+            ratingDescription: ratingDescription,
+            target: target,
+            average: average
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
     }
-    calcTraining();
 
-
-
-    return {
-        periodLength: periodLength,
-        trainingDays: trainingDays.length,
-        success: success,
-        rating: rating,
-        ratingDescription: ratingDescription,
-        target: target,
-        average: average
-    }
 }
 
-console.log(calculateExercises([1,2,3,4,5,6,7], 4))
+const target: number = Number(process.argv[2])
+const arr: Array<number> = process.argv.slice(3).map(Number)
+
+
+try {
+    console.log(calculateExercises(target, arr))
+  } catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
